f598360270e8eb21c15ca775f8c99a5ca071ff59
Tyler washington <lilturtleneckbeats@gmail.com>
2023-10-13 19:19:22 -0400

    rearranged folder structure

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from classes.board import Othello_Board
+from board.board import Othello_Board
 
 def main():
     othello_board = Othello_Board()
-    othello_board.board[3][5] = 'b'
     othello_board.print_board()
 
 if __name__ == "__main__":

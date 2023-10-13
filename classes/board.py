4f95adb32e72f1143014fe2f27780f842aea2bc0
Tyler washington <lilturtleneckbeats@gmail.com>
2023-10-13 19:08:45 -0400

    Updated print board function

--- classes/board.py
+++ classes/board.py
@@ -1,7 +1,8 @@
 class Othello_Board:
     def __init__(self):
-        self.board = [['', '', '', '', '', '', '', ''] for _ in range(8)]
+        self.board = [[' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] for _ in range(8)]
 
     def print_board(self):
         for row in self.board:
-            print(row)
+            print("|".join(row))
+            print('-' * 15)
